05e196136868570ec53c5187394c5a1a434fdf37
Hart-Luna <carol.harryanddan@gmail.com>
2024-01-27 17:17:21 -0300

    editcs

--- src/header/header.js
+++ src/header/header.js
@@ -7,24 +7,25 @@ import search from '../assets/icons/search.png';
 const Header = () = {
     return (
         <nav className="header__navigation">
-            <div className="navigation">
-                <button className="arrow-left">
-                    <img src={smallLeft} alt="arrow-left">
-                    </></button>
+        <div className="navigation">
+            <button className="arrow-left">
+                <img src={smallLeft} alt="arrow-left"/>
+            </button>
                 <button className="arrow-right">
-                    <img src={smallRight} alt="arrow-right">
-                    </></button>
-            </div>
-            <div classNameName="header__search">
-                <img src={search} alt="search">
-                    <input id="search-input" type="text" maxlength="800" placeholder="Listen to something new.">
-                    </div>
-                    <div className="header__login">
-                        <button className="subscribe">Subscribe</button>
-                        <button className="login">Login</button>
-                    </div>
-                </nav>
+                    <img src={smallRight} alt="arrow-right"/>
+                </button>
 
+            <div classNameName="header__search">
+                    <img src={search} alt="search">
+                    <input id="search-input" type="text" maxlength="800" placeholder="Listen to something new.">
+            </div>
+        </div>
+
+        <div className="header__login">
+                <button className="subscribe">Subscribe</button>
+                <button className="login">Login</button>
+        </div>
+        </nav>
                 );
 }
 
